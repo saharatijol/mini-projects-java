bd40d1e35be16052d5cff1e73aa366bbe5737550
sahara <sahara.tijol@gmail.com>
2025-10-23 12:44:39 -0500

    read, parse and store each transactions into objects

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
+
+    public static final String FILE = "data/transactions.csv";
+
     public static void main(String[] args) {
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("data/transactions.csv"))) {
+        ArrayList<Transaction> transactionsList = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(FILE))) {
+
             String line;
             reader.readLine(); // skips header
             while((line = reader.readLine()) != null) {
-                System.out.println(line);
+                String[] arr = line.split(",");
+                double amount = Double.parseDouble(arr[2]);
+                transactionsList.add(new Transaction(arr[0], arr[1], amount));
             }
-            //System.out.println(reader.readLine());
+
+            for(Transaction transaction : transactionsList) {
+                System.out.println(transaction.getDate() + "     "
+                                    + transaction.getDescription() + "     "
+                                    + transaction.getAmount());
+            }
+
+
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.err.println("ERROR: " + FILE + " not found in data/ folder");
+
         } catch (IOException e) {
+            System.err.println("ERROR: Could not read file - " + e.getMessage());
             e.printStackTrace();
         }
     }
