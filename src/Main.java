aa6d52dc436629912e07d2ca6ad796105e0cde11
sahara <sahara.tijol@gmail.com>
2025-10-23 14:48:50 -0500

    parses and categorizes. its functions okay but  the code could be cleaner. need to fix repetitive operations

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
@@ -22,13 +24,6 @@ public class Main {
                 transactionsList.add(new Transaction(arr[0], arr[1], amount));
             }
 
-            for(Transaction transaction : transactionsList) {
-                System.out.println(transaction.getDate() + "     "
-                                    + transaction.getDescription() + "     "
-                                    + transaction.getAmount());
-            }
-
-
         } catch (FileNotFoundException e) {
             System.err.println("ERROR: " + FILE + " not found in data/ folder");
 
@@ -36,5 +31,83 @@ public class Main {
             System.err.println("ERROR: Could not read file - " + e.getMessage());
             e.printStackTrace();
         }
+
+
+        // Print Transaction objects
+        for(Transaction transaction : transactionsList) {
+            System.out.println(transaction.getDate() + "     "
+                    + transaction.getDescription() + "     "
+                    + transaction.getAmount());
+        }
+
+        // Categorize Expenses
+        // keywords to find and categorize
+        System.out.println("\nCategorize Items:");
+        System.out.println("Expense Breakdown:");
+        ArrayList<Transaction> groceries = new ArrayList<>();
+        ArrayList<Transaction> gasList = new ArrayList<>();
+        ArrayList<Transaction> entertainList = new ArrayList<>();
+        String description = "";
+
+        // Groceries
+        System.out.println("Groceries:");
+        for(Transaction transaction : transactionsList) {
+            description = transaction.getDescription();
+            if(description.contains("Grocery") || description.contains("Market")) {
+                groceries.add(transaction);
+                System.out.println(description + "     " + transaction.getAmount());
+            }
+        }
+
+        // Gas
+        System.out.println("\nGas:");
+        for(Transaction transaction : transactionsList) {
+            description = transaction.getDescription();
+            if(description.contains("Gas")) {
+                gasList.add(transaction);
+                System.out.println(description + "     " + transaction.getAmount());
+            }
+        }
+
+        // Entertainment
+        System.out.println("\nEntertainment:");
+        for(Transaction transaction : transactionsList) {
+            description = transaction.getDescription();
+            if(description.contains("Netflix") || description.contains("HBO") ||
+                    (description.contains("Spotify") || description.contains("Steam"))) {
+                entertainList.add(transaction);
+                System.out.println(description + "     " + transaction.getAmount());
+            }
+        }
+
+
+        HashMap<String, Double> expenses = new HashMap<>();
+        double groceryTotal = 0;
+        double gasTotal = 0;
+        double entTotal = 0;
+
+        for(Transaction grocery : groceries) {
+            groceryTotal += grocery.getAmount();
+        }
+
+        for(Transaction gas : gasList) {
+            gasTotal += gas.getAmount();
+        }
+
+        for(Transaction entertainment : entertainList) {
+            entTotal += entertainment.getAmount();
+        }
+
+
+        expenses.put("Grocery", groceryTotal);
+        expenses.put("Gas", gasTotal);
+        expenses.put("Entertainment", entTotal);
+
+        System.out.println("\nTotal Spent:");
+        for(Map.Entry<String, Double> entry : expenses.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
     }
 }
